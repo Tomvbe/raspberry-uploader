3e57aac8e33254c079e30c388bc507b4e9e23ea2
Tom Ryzen <tom.vanbesien@gmail.com>
2021-05-16 20:22:05 +0200

    Avoid race condition for dir creation

--- src/main/java/com/raspberry/uploader/service/FileService.java
+++ src/main/java/com/raspberry/uploader/service/FileService.java
@@ -1,7 +1,6 @@
 package com.raspberry.uploader.service;
 
 import com.raspberry.uploader.exception.FileStorageException;
-import lombok.Synchronized;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -34,9 +33,16 @@ public class FileService {
         Arrays.asList(files).forEach(file -> uploadFile(file, absoluteDir));
     }
 
-    private void createDirectory(Path dirPath) {
+    /**
+     * Uploads come in async so it possible for two thread to create a new directory at the same time.
+     * Technically creating a new directory a second time is only a problem when there is already content in the dir.
+     * Better safe than sorry :)
+     */
+    synchronized private void createDirectory(Path dirPath) {
         try {
-            Files.createDirectory(dirPath);
+            if (Files.notExists(dirPath)) {
+                Files.createDirectory(dirPath);
+            }
         } catch (IOException e) {
             throw new FileStorageException("Could not create a new directory for the files", e);
         }
